d1a19864bfaa5818271096707685f2c08ae1d4f3
Guillermo Herrera Garcia <guihega@live.com.mx>
2025-11-07 12:22:43 -0600

    Fix rutas Vite + Astro para subcarpeta /venados-atalaya/

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mhegasdev.com.mx',
-  base: '/venados-atalaya/',
+  base: '/venados-atalaya/', // 👈 Muy importante: barra inicial y final
   output: 'static',
-  outDir: './dist',  // 👈 fuerza a usar esta carpeta
+  outDir: './dist',
+
+  // ⚙️ Ajuste extra para Vite: asegura que los assets (CSS/JS/IMGS) respeten la subcarpeta
+  vite: {
+    base: '/venados-atalaya/',
+  },
 });
